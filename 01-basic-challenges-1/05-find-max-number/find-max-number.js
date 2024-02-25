dca8e99adfff3f3bf04756806ae70cea34fc20e6
Geoffrey Kithuku <mutembeikithuku12@gmail.com>
2024-02-25 18:29:43 +0300

    solve title case

--- 01-basic-challenges-1/05-find-max-number/find-max-number.js
+++ 01-basic-challenges-1/05-find-max-number/find-max-number.js
@@ -1,5 +1,19 @@
 function findMaxNumber(arr) {
-    return Math.max(arr)
+    // solution 1
+    // return Math.max(...arr);
+
+
+
+    // solution 2
+    let max = arr[0];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+
+    return max;
 }
 
 module.exports = findMaxNumber;
--- 01-basic-challenges-1/06-title-case/title-case.js
+++ 01-basic-challenges-1/06-title-case/title-case.js
@@ -1,3 +1,12 @@
-function titleCase() {}
+function titleCase(str) {
+  let words = str.split(" ");
+  let capitalised = words.map((word) =>
+    word.toLowerCase().replace(word[0], word[0].toUpperCase())
+  );
+
+  capitalised = capitalised.join(" ");
+
+  return capitalised;
+}
 
 module.exports = titleCase;
