fb247d987fa637d6a14fa8300558c433486e28a4
Geoffrey Kithuku <mutembeikithuku12@gmail.com>
2024-02-25 19:59:50 +0300

    solve vowels count in a string

--- 01-basic-challenges-1/09-count-vowels/count-vowels.js
+++ 01-basic-challenges-1/09-count-vowels/count-vowels.js
@@ -1,3 +1,14 @@
-function countVowels() {}
+function countVowels(str) {
+    const vowels = 'aeiou'
+    let vowelsCount = 0
+
+    for (let i = 0; i < str.length; i++){
+        if(vowels.includes(str[i].toLowerCase())){
+            vowelsCount++
+        }
+    }
+
+    return vowelsCount
+}
 
 module.exports = countVowels;
