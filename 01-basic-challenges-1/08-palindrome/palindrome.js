b120bd4b40ff8a6e723669c02094484f1dc08568
Geoffrey Kithuku <mutembeikithuku12@gmail.com>
2024-02-25 19:27:14 +0300

    solve palidrome

--- 01-basic-challenges-1/08-palindrome/palindrome.js
+++ 01-basic-challenges-1/08-palindrome/palindrome.js
@@ -1,3 +1,23 @@
-function isPalindrome() {}
+function isPalindrome(str) {
+   
+    function reverseString(str) {
+        let reversed = ''
+        for (let i = str.length - 1; i >= 0; i--) {
+            if (str[i] === ' ' || str[i] == ',') {
+                continue
+            }
+            
+            reversed += str[i]
+        }
+        return reversed.toLowerCase()
+    }
+
+    let reversed = reverseString(str)
+    let original = reversed.split('').reverse().join('')
+
+    return reversed === original
+
+}
 
 module.exports = isPalindrome;
+
